f955e48a22b3e4b8ee95477b0bb1489db242c301
Provd5 <provd12345@gmail.com>
2025-05-18 20:32:52 +0200

    added translation to validation

--- app/Http/Controllers/DynamicFormController.php
+++ app/Http/Controllers/DynamicFormController.php
@@ -12,6 +12,13 @@ class DynamicFormController extends Controller
             'name' => ['required', 'string', 'min:3', 'max:50'],
             'email' => ['required', 'email'],
             'description' => ['nullable', 'string', 'max:500'],
+        ], [
+            'name.required' => 'Pole imię jest obowiązkowe.',
+            'name.min' => 'Pole imię musi mieć co najmniej :min znaki.',
+            'name.max' => 'Pole imię nie może mieć więcej niż :max znaków.',
+            'email.required' => 'Pole email jest obowiązkowe.',
+            'email.email' => 'Pole email musi być poprawnego formatu.',
+            'description.max' => 'Opis może mieć maksymalnie :max znaków.',
         ]);
 
         return redirect()->back()->with([
